2863d160248e2be4b3b192b732e4e2b985936d77
ajeetkumarukande <108564439+ajeetkumarukande@users.noreply.github.com>
2024-02-15 20:29:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from streamlit_chat import message
+from utils import *
+
+st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+response_container = st.container()
+textcontainer = st.container()
+
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            context = find_match(query)
+            response = conversation.predict(input=f"Context:\n{context}\n\nQuery:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+
+with response_container:
+    if st.session_state['responses']:
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
